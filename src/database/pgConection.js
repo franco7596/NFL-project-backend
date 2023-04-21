6e8b764f602d80b0f11e21285196990f91cb95a8
franco7596 <franco_7_5_96@hotmail.com>
2023-04-21 17:50:39 -0300

    changin the conecction to database

--- src/database/pgConection.js
+++ src/database/pgConection.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-	host: "localhost",
-	user: "postgres",
-	password: "Nfl*2023",
-	database: "NFLDatabase",
+  host: "localhost",
+  user: process.env.USER || "",
+  password: process.env.PASSWORD || "",
+  database: process.env.DATABASE || "",
 });
 
 exports.pool = pool;
